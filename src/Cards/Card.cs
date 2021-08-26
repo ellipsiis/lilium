c28a2f196f84cc2d9758cb9a4e0047d0e1a39521
Daniel Zarco <ellipsiis@debian-BULLSEYE-live-builder-AMD64>
2021-08-26 12:45:45 -0500

    feat(card): adding fields
    adding first approach of fiels to object card into region

---
+++ src/Cards/Card.cs
@@ -0,0 +1,25 @@
+using System;
+namespace lilium.src.Cards
+{
+    public class Card
+    {
+        #region field of class
+        private int _id;
+        private string _number;
+        private int _nip;
+        private string _nameClient;
+        private string _lastnameClient;
+        private string brand;
+        private string country;
+        private int _expiresMonth;
+        private int _expiresYear;
+        private string _funding;
+        private string _lastDigits;
+        private string _fingerPrint;
+        #endregion
+
+        #region constructors
+        
+        #endregion
+    }
+}
